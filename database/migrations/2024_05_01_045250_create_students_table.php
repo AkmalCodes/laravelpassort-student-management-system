203e4f96f481d5d910d08b6ab5e958f3de658ed8
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-05-01 13:09:56 +0800

    updated migration file for user_id fields in student table

--- database/migrations/2024_05_01_045250_create_students_table.php
+++ database/migrations/2024_05_01_045250_create_students_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string('student_name');
             $table->string('student_email')->unique();
             $table->string('student_address');
             $table->string('student_study_course');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
