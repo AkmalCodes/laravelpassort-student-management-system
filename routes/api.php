19b2efe990a00f8761e595019c68014deee03f0f
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-05-12 13:43:27 +0800

    beautified code structure

--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
-
-
 Route::post("register", [UserController::class, "register"]);
 Route::post("login", [UserController::class, "login"]);
 
 Route::group([
     "middleware" => ["auth:api"]
 ],function(){
+    //user controllers
+    Route::get('user', function (Request $request) {return $request->user();});
     Route::get("profile", [UserController::class, "profile"]);
     Route::get("logout", [UserController::class, "logout"]);
+
+    //student controllers
     Route::post("search_students_email", [StudentController::class, "search_students_email"]);
     Route::post("search_students_name", [StudentController::class, "search_students_name"]);
 });
