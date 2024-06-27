7226def9043a409b46cc31d9c8e74bb089645299
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-06-27 16:24:02 +0800

    added roles table and added role_id field in user table

--- database/migrations/0001_01_01_000000_create_user_table.php
+++ database/migrations/0001_01_01_000000_create_user_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('user_type');
+            $table->integer('role_id')->default(2);
             $table->rememberToken();
             $table->timestamps();
         });
